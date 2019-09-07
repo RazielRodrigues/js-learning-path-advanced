ae4218f1b3eb17a04a947df72116d5d7cb617e15
Raziel Miranda <raziel.miranda@galata.com.br>
2019-09-07 20:19:51 -0300

    replace string

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-alert("oi");
+var nome = "Raziel Miranda";
+var idade = 20;
+var kg = 65;
+var altura = 1.70;
+var anime = "Fairy tail é muito legal";
+
+console.log((idade * altura) / altura);
+console.log("O " + nome + " Gosta muito de " + anime);
+console.log("O " + nome.replace("Raziel", "Igor") + " gosta muito de "
+ + anime.replace("Fairy tail", "Dragonball"));
+
