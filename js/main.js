cbdeaab972486323669e4a5b19f110efe9ff2aec
Raziel Miranda <raziel.miranda@galata.com.br>
2019-09-07 20:34:55 -0300

    mais metodos com array

--- js/main.js
+++ js/main.js
@@ -2,5 +2,10 @@ var fruteira = ["Banana", "Mexirica", "Melão"];
 
 fruteira.push("Melancia");
 console.log(fruteira);
+
 fruteira.pop("Banana");
 console.log(fruteira);
+
+console.log(fruteira.toString());
+console.log(fruteira.join(" - "));
+console.log(fruteira.length);
