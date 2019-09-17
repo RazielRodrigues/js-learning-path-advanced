38a2df6272ba53a6162a1a64283d8cd8249effe8
Raziel Miranda <raziel.miranda@galata.com.br>
2019-09-16 23:44:42 -0300

    Update main.js

---
+++ js/main.js
@@ -0,0 +1,21 @@
+String letras = 'Temos 6 tipos de variaveis no ECMA 6';
+Number numero = 6;
+Boolean verdade = true;
+
+//Dá pra quase criar um enull do typescript.
+Symbol valorUnico = 'Esse é um valor único'; 
+
+//Representa o nulo.
+null variavelPelada;
+
+//Existe mais ainda não foi definida.
+undefined variavelIndecisa; 
+
+//Dicionario de dados etc...
+Object Objeto
+
+//É um "objeto" mas permite que a gente possa chama-lo.
+function () {}
+
+//O Array tem relação com seus "filhos" ou seja os seus valores.
+Array vetorzinho[];
