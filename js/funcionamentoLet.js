ee04da8cf9f91f371d5916482daa593cfdfbd599
Raziel Miranda <raziel.miranda@galata.com.br>
2019-09-16 23:31:53 -0300

    funcionamento const

--- js/funcionamentoLet.js
+++ js/funcionamentoLet.js
@@ -1,11 +1,9 @@
 function funcionamentoLet() {
 
 	let escopoBlocoIntro = 'Variaveis que são declaradas com let tem espoco de bloco, ';
-	let escopoBlocoMeio = 'significa que ela existe apenas dentro de chaves ';
-	let escopoBlocoFinal = 'ou seja em funções, if, laços, dicionarios de dados etc...';
-	
+	let escopoBlocoMeio = 'significa que ela existe apenas dentro de chaves';
+	let escopoBlocoFinal = 'ou seja dentro de { }, pode ser em funções, if, laços, dicionarios de dados etc...';
 	console.log(escopoBlocoIntro + escopoBlocoMeio + escopoBlocoFinal);
-
 		if (true) {
 			let escopoBlocoSemAcesso = 'Ela também não pode ser acessada fora dos { }';
 		}
@@ -13,4 +11,4 @@ function funcionamentoLet() {
 	console.log(escopoBlocoSemAcesso);
 }
 
-funcionamentoLet();
+ funcionamentoLet();
