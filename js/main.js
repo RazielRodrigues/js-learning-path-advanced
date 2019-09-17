f4b349964e3897d0ed146ae49d1f4f199dad873e
Raziel Miranda <raziel.miranda@galata.com.br>
2019-09-16 23:03:59 -0300

    anotações

---
+++ js/main.js
@@ -0,0 +1,40 @@
+function mostrarNome() {
+
+	console.log(nome);
+
+	var nome = 'Raziel M.';
+
+	console.log(nome);
+
+}
+
+mostrarNome();
+
+function mostrarIdade() {
+	var idade = 19;
+
+	console.log(idade);
+
+	idade + 1 = console.log(idade);
+
+}
+
+mostrarIdade();
+
+/*
+
+Quando voce declara uma variavel no JS
+voce consegue usar ela antes ou depois de sua declaração
+esse é o hosting de variavel. Eu usei a variavel antes
+de eu declarar ela ou seja, ela está no final mas eu consigo
+usa ela no inicio, entretanto vai retornar como indefinida
+pois ela existe mais ainda não foi usada.
+
+No segundo exemplo eu declaro apenas uma vez a variavel
+e o seu respectivo tipo, pois, voce defini o tipo da variavel
+quando declara ela, não sendo necessário eu citar o tipo da 
+variavel denovo abaixo e nem declarar ela novamente.
+
+
+
+*?
