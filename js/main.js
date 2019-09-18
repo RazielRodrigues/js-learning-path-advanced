6600c9ed91b4350c82efe8ba6ce1531333afb600
Raziel Miranda <raziel.miranda@galata.com.br>
2019-09-18 10:54:43 -0300

    sintaxe de classe e de encapsulamento

---
+++ js/main.js
@@ -0,0 +1,3 @@
+function cachorro(argument) {
+	// body...
+}
--- js/orientacaoObjetos.js
+++ js/orientacaoObjetos.js
@@ -18,6 +18,7 @@ function ComoFuncionaObjeto(MeuNome) {
 
 const x = new ComoFuncionaObjeto('Raziel M.');
 console.log(x);
+
 /*
 
 1 - Se cria um novo objeto e ele herda o prototipo de Foo
@@ -27,3 +28,57 @@ e com o 'this' vinculando ao novo objeto criado
 o seu return, senão, será retornado o objeto criado no passo 1.
 
 */
+
+//Sintaxe de orientaçãop a objetos em ECMA 6
+//Para gerar classes e herança
+class Animal{
+	constructor(){
+		this.qtdepatas = 0 ;
+	}
+
+	movimentar();
+}
+
+class cachorro extends animal{
+	constructor(morde){
+		super();
+		this.qtdepatas = 4;
+		this.morde = morde;
+	}
+
+	latir(){
+		console.log('au au');
+	}
+
+}
+
+const pug = new cachorro(false);
+const pitbull = new cachorro(true);
+
+
+
+
+//Trabalhando com modificadores de acesso
+// e o encapsulamento
+class person{
+	#name = '';
+
+	constructor(nomeinicial){
+		this.#name = nomeinicial;
+	}
+
+	setname(name){
+		this.#name = name;
+	}
+
+	getname(){
+		return this.#name;
+	}
+}
+
+console.log(p);
+p.getname();
+p.name();
+
+p.setname('Raziel M.');
+p.getname();
