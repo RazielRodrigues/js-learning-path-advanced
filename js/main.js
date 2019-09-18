2852176f361f667d95cad4506121f16488fbee72
Raziel Miranda <raziel.miranda@galata.com.br>
2019-09-18 11:55:08 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
-function cachorro(argument) {
-	// body...
-}
+function 
+
+Pessoa(nome, idade) { 
+
+this.nome = nome;
+
+ this.idade = idade; 
+
+ return { nome, idade: 20, 
+
+
+ falar() { 
+
+
+ console.log('objeto falar'); 
+ } 
+ } 
+ } 
+
+
+ Pessoa.prototype.falar = function() {
+
+  console.log('prototype falar'); }; 
+
+
+  const pessoa = new Pessoa('Foo', 30); 
+
+  Com base no código acima, observe as afirmativas abaixo:
