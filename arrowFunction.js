270acbe6b247b66c977839ff382ec8e37ec9b9f0
Raziel M. Rodrigues <raziel.rodrigues@fatec.sp.gov.br>
2019-12-03 17:16:16 +0000

    Update arrowFunction.js

--- arrowFunction.js
+++ arrowFunction.js
@@ -1,37 +1,43 @@
 //Maneira clássica
 
-function log(value){
+function logClassica(value){
 	console.log(value);
 }
-
-log('Funcão clássica');
+logClassica('Funcão clássica');
 
 //Função anonima: omitir o nome da função atribuindo ela a uma variavél ou para outra função
 
-var log = function(value){
+var logAnonima = function(value){
 	console.log(value);
 }
-
 log('Função anonima');
 
-//Com return
+//Função anonima: com return
 
-var sum = function(a,b){
+var sumAnomima = function(a,b){
 	return a + b;
 }
-
-console.log(sum(10,10));
-
-
-//Arrow functions, mesmo principio de uma função anonima
+console.log(sumAnomima(10,10));
 
 
+//Arrow functions: mesmo principio de uma função anonima
 
+//Quando não tem statement "if else e variaveis" pode se escrever assim:
+//Basicamente o que se faz é omitir a palavra funcion, return e chaves
 
+var arrowSemStatement = (a,b) => a + b;
+console.log(arrowSemStatement(10,5));
 
+//Quando tem statements é obrigatorio escrever com chaves
+//Basicamente o que se faz é omitir a palavra funcion
 
-
-
+var arroComStatement = (a,b) =>{
+	if (a > b) {
+		console.log('a é maior que b');
+	}
+	return a + b;
+}
+arroComStatement(10,5);
 
 
 
