c6d9aa9361e54fdf74d2f7582a67924208783e98
Raziel M. Rodrigues <raziel.rodrigues@fatec.sp.gov.br>
2019-12-04 14:33:00 +0000

    arrow functions e spread operators

--- restSpreadOperator.js
+++ restSpreadOperator.js
@@ -24,13 +24,69 @@ console.log(sum(1,2,3,4,5));
 //enquanto o arguments retorna os argumento da função em forma
 //de objeto.
 
-function sum(...vetorArgumento){
-	console.log(vetorArgumento);
-	console.log(arguments);
+function sum(...restOperator){
+	console.log(restOperator);
+	//console.log(arguments);
 	//return vetorArgumento.pop();
+	//return restOperator;
 }
 
 sum(1,2,3,4,5);
+
+//Rest operators e arrow functions parte 1:
+//Com arrow functions o arguments não funciona, mas o restoperator sim
+var sum = (...restOperator) => {
+	console.log(restOperator);
+};
+
+console.log(sum(1,2,3,4,5));
+
+
+//Rest operators e arrow functions parte 2:
+//ele também funciona com argumentos definidos e usa o restante como restoperator
+//a parte definida sem o rest operator retorna como os valores passados normal
+//a parte do rest operator retorna como array.
+var sum = (a,b,...restOperator) => {
+	console.log(a,b,restOperator);
+};
+
+console.log(sum(1,2,3,4,5));
+
+
+//O Spread operator ele serve para o return da função,
+//Ele pega o argumento de uma outra função trasnforma tudo em uma lista
+//e pode ser usado como argmentos no return de outra função.
+//também pode ser usado com: Strings, arrays, objects literais e objectos iteraveis
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
