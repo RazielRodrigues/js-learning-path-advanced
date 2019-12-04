05dfea7f66601f46fbb908d600b534d623633d56
Raziel M. Rodrigues <raziel.rodrigues@fatec.sp.gov.br>
2019-12-04 03:44:26 +0000

    contexto de invocação!

--- arrowFunction.js
+++ arrowFunction.js
@@ -79,4 +79,118 @@ console.log(new naofunc());
 
 
 
+//CONTEXTO DE INVOCAÇÃO PT.1
+//O THIS REVERENCIA O PROPRIO OBJETO E UAS PROPRIEDAES 
+//POIS, PEGA FORA DO CONTEXTO DAS CHAVES
+var obj = {
+
+	showContext: function showContext(){
+		console.log(this);
+	}, 
+
+	log: function log(value){
+		console.log(value);
+	}
+
+
+};
+
+//QUANDO SE OLHA PRA ESQUERDA PERCEBE-SE O OBJETO QUE O THIS IRIA 
+//REVERENCIAR, UMA DICA QUE USAVA.
+obj.showContext();
+
+
+
+
+
+//CONTEXTO DE INVOCAÇÃO PT.2
+//É POSSIVEL CHAMAR A FUNÇÃO DO OBJETO ABAIXO POIS PEGA DO CONTEXTO FORA DAS CHAVES 
+//Mas é necessario colocar o this para reverenciar o objeto em si sem o this nao reconhece
+var obj = {
+
+	showContext: function showContext(){
+		this.log('Chamando função abaixo por conta que pega o contexto de fora da chave ');
+	}, 
+
+	log: function log(value){
+		console.log(value);
+	}
+
+
+};
+
+obj.showContext();
+
+
+//CONTEXTO DE INVOCAÇÃO PT.3
+//Declarando uma função de settimeout de nivel global do browser, 
+//e chamando a log dentro dela
+//de qualquer forma não funcionaria porque o contexto global de eventlistener
+//seria o Browser object model então o this buscaria uma função nesse contexto
+
+var obj = {
+
+	showContext: function showContext(){
+		this.log('Chamando função abaixo por conta que pega o contexto de fora da chave ');
+
+		setTimeout(function(){
+			console.log(this);
+			//this.log('Set time out!');
+		}, 3000);
+
+	}, 
+
+	log: function log(value){
+		console.log(value);
+	}
+
+
+};
+
+obj.showContext();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
