43505ba376d0954852838de181ba5920c7dcc836
Raziel M. Rodrigues <raziel.rodrigues@fatec.sp.gov.br>
2019-12-04 14:13:09 +0000

    rest operator

--- restSpreadOperator.js
+++ restSpreadOperator.js
@@ -1,4 +1,4 @@
-//Spread operator serve para quando voce deseja
+//Rest operator serve para quando voce deseja
 //Incrementar os argumentos  de uma função sem saber
 //Quantos argumentos ela vai ter e deseja que ocorra
 //o comportamento esperado da função independente dos paramentros que
@@ -18,4 +18,29 @@ function sum(a,b){
 console.log(sum(1,2,3,4,5));
 
 
-//Com ECMA 6 
+//Com ECMA 6
+//Ele simula o comportamento do arguments mas já trazendo em forma de vetor
+//podendo ser usado os metodos de vetor para manipular os paramentros.
+//enquanto o arguments retorna os argumento da função em forma
+//de objeto.
+
+function sum(...vetorArgumento){
+	console.log(vetorArgumento);
+	console.log(arguments);
+	//return vetorArgumento.pop();
+}
+
+sum(1,2,3,4,5);
+
+
+
+
+
+
+
+
+
+
+
+
+
