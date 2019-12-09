18b53932970f18d84f3a7f336d1cdc36f034104f
Raziel M. Rodrigues <raziel.rodrigues@fatec.sp.gov.br>
2019-12-09 01:08:21 +0000

    Update symbolsInterators.js

--- symbolsInterators.js
+++ symbolsInterators.js
@@ -26,3 +26,24 @@ Symbol.split
 Symbol.toPrimitive
 Symbol.asyncIterator
 
+
+//O iterator percorre todo o array usando a função next
+//e quando não tem mais valor o parametro done da função retorna true
+//Basicamente o que faz é ir exibindo todos os valores do array
+const arr = [1,2,3];
+const it = arr[Symbol.iterator]();
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
+
+//Esse for serve para percorrer os array e devolver qual valor tem
+for (let value of arr){
+console.log(value);
+
+}
+
+
+
+
+
