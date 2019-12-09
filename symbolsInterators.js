cfefa7e110f77c26f001222c6ef4b6ba76729880
Raziel M. Rodrigues <raziel.rodrigues@fatec.sp.gov.br>
2019-12-09 00:47:01 +0000

    oq ue é symbol e well known

--- symbolsInterators.js
+++ symbolsInterators.js
@@ -1,1 +1,28 @@
 //O simbolo não é uma string e também não é um número, é apenas um identifcador único.
+//como o ID de um banco de dados
+
+const uniqueId = Symbol('Hello');
+const uniqueId2 = Symbol('Hello');
+
+//O symbol é diferente mesmo se o valor for igual
+//return false
+console.log(uniqueId === uniqueId2);
+
+
+//Ele serve também para deixar uma propriedade do objeto
+//inacesseivel por metodos convencionais
+//e tambem serve para que outros dev entendeam que nao pode mecher ali
+
+var obj = {
+	[uniqueId] : 'Hello'
+}
+
+console.log(obj);
+
+//Well known symbols:
+
+Symbol.iterator
+Symbol.split
+Symbol.toPrimitive
+Symbol.asyncIterator
+
