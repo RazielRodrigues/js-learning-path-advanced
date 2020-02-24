51ba80faccdf13dc6eb9145eadb8a571e44cae66
Raziel M. Rodrigues <raziel.rodrigues@fatec.sp.gov.br>
2020-02-23 23:15:22 -0300

    requisição ajax em uma api do via cep com formatação em jquery

--- js/main.js
+++ js/main.js
@@ -1,3 +1,30 @@
 function consultaCep(){
 
+	var	inputCep = document.getElementById('inputCep').value;
+	var url = "https://viacep.com.br/ws/"+inputCep+"/json/";
+	console.log(url);
+	$.ajax({
+		url: url,
+		type: "GET",
+		success: function(response){
+			console.log(response);
+
+			$('#cep').html(response.cep);
+			//document.getElementById('cep').innerHTML = response.cep;
+			document.getElementById('logradouro').innerHTML = response.logradouro;
+			document.getElementById('complemento').innerHTML = response.complemento;
+			document.getElementById('bairro').innerHTML = response.bairro;
+			document.getElementById('localidade').innerHTML = response.localidade;
+			document.getElementById('uf').innerHTML = response.uf;
+
+		}
+	});
+
 }
+
+/*
+
+Jquery facilita no uso de requisições ajax e tambem com diversos metodos e funções
+que auxiliam na escrita do código.
+
+*/
