8c10acfef04bbbec927b74ded212780289a1e980
Raziel M. Rodrigues <raziel.rodrigues@fatec.sp.gov.br>
2020-02-23 23:41:44 -0300

    melhorando front-end e funções do ajax

--- js/main.js
+++ js/main.js
@@ -3,20 +3,12 @@ function consultaCep(){
 	var	inputCep = document.getElementById('inputCep').value;
 	var url = "https://viacep.com.br/ws/"+inputCep+"/json/";
 	console.log(url);
+	
 	$.ajax({
 		url: url,
 		type: "GET",
 		success: function(response){
 			console.log(response);
-
-			$('#cep').html(response.cep);
-			//document.getElementById('cep').innerHTML = response.cep;
-			document.getElementById('logradouro').innerHTML = response.logradouro;
-			document.getElementById('complemento').innerHTML = response.complemento;
-			document.getElementById('bairro').innerHTML = response.bairro;
-			document.getElementById('localidade').innerHTML = response.localidade;
-			document.getElementById('uf').innerHTML = response.uf;
-
 		}
 	});
 
