84924d8605d79c13d4c32f41a1a4a4af782d653f
Raziel Rodrigues <raziel@upoutsourcing.com>
2021-06-20 02:44:31 -0300

    flappy part 2

--- dom/js/flappy.js
+++ dom/js/flappy.js
@@ -18,3 +18,30 @@ function Barreira(reversa = false) {
 // const b = new Barreira(true);
 // b.setAltura(200);
 // document.querySelector('[wm-flappy]').appendChild(b.elemento);
+
+function ParDeBarreiras(altura, abertura, x) {
+    this.elemento = novoElemento('div', 'par-de-barreiras');
+
+    this.superior = new Barreira(true);
+    this.inferior = new Barreira(false);
+
+    this.elemento.appendChild(this.superior.elemento);
+    this.elemento.appendChild(this.inferior.elemento);
+
+    this.sortearAbertura = () => {
+        const alturaSuperior = Math.random() * (altura - abertura);
+        const alturaInferior = altura - abertura - alturaSuperior;
+        this.superior.setAltura(alturaSuperior);
+        this.inferior.setAltura(alturaInferior);
+    }
+
+    this.getX = () => parseInt(this.elemento.style.left.split('px')[0]);
+    this.setX = x => this.elemento.style.left = `${x}px`;
+    this.getLargura = () => this.elemento.clientWidth;
+
+    this.sortearAbertura();
+    this.setX(x);
+}
+
+const b = new ParDeBarreiras(500, 200, 800);
+document.querySelector('[wm-flappy]').appendChild(b.elemento);
